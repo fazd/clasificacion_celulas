fd1fc8b375e9ee98f90311f3318edda5921df7e4
Fazd <fabioandres2907@hotmail.com>
2017-11-16 00:58:54 -0500

    add new Directory, format correct

--- clasificacion_fase.py
+++ clasificacion_fase.py
@@ -2,14 +2,16 @@ import numpy as np
 from operator import itemgetter
 import cv2
 
-class Clasificador:
-    def __init__(self, imagen,nombre, distancias,colores):
-        self.__img=imagen
-        self.__name=nombre
-        self.__dist_lista=distancias
-        self.__col_lista=colores
 
-    def guardar_imagen(self,tipo):
+class Clasificador:
+
+    def __init__(self, imagen, nombre, distancias, colores):
+        self.__img = imagen
+        self.__name = nombre
+        self.__dist_lista = distancias
+        self.__col_lista = colores
+
+    def guardar_imagen(self, tipo):
         k = tipo+'/imagen' + str(self.__name) + '.png'
         cv2.imwrite(k, self.__img)
 
@@ -26,135 +28,133 @@ class Clasificador:
 
     def telofase(self):
         var = self.varianza()
-        if(var>=6.5):
+        if var >= 6.5:
             self.guardar_imagen('telofase')
-            print('la',self.__name,'está en telofase')
+            #print('la',self.__name,'está en telofase')
             return True
         return False
 
     def find_min(self):
-        min_X=1000
-        min_Y=1000
-        k=len(self.__col_lista)
-        for i in range(k):
-
-            x,y=self.__col_lista[i]
-            if(x<min_X):
-                min_X=x
-            if(y<min_Y):
-                min_Y=y
-        return min_X,min_Y
-
-    def find_max(self):
-        max_X = -10
-        max_Y = -10
+        min_x = 1000
+        min_y = 1000
         k = len(self.__col_lista)
         for i in range(k):
             x, y = self.__col_lista[i]
-            if (x >max_X):
-                max_X = x
-            if (y > max_Y):
-                max_Y = y
-        return max_X,max_Y
+            if x < min_x:
+                min_x = x
+            if y < min_y:
+                min_y = y
+        return min_x, min_y
+
+    def find_max(self):
+        max_x = -10
+        max_y = -10
+        k = len(self.__col_lista)
+        for i in range(k):
+            x, y = self.__col_lista[i]
+            if x > max_x:
+                max_x = x
+            if y > max_y:
+                max_y = y
+        return max_x, max_y
 
     def find_centroid_cell(self):
-        x_min,y_min=self.find_min()
-        x_max,y_max=self.find_max()
-        x_centroid=int((x_max+x_min)/2)
-        y_centroid=int((y_max+y_min)/2)
-        centroide=x_centroid,y_centroid
+        x_min, y_min = self.find_min()
+        x_max, y_max = self.find_max()
+        x_centroid = int((x_max+x_min)/2)
+        y_centroid = int((y_max+y_min)/2)
+        centroide = x_centroid, y_centroid
         #print('centroide=',centroide)
         return centroide
 
     def obtener_bordes(self,img,lista):
-        lista=sorted(lista,key=itemgetter(1))
-        __,max_y=max(lista,key=itemgetter(1))
-        __,min_y=min(lista,key=itemgetter(1))
-        lim=len(lista)
-        copia=img
-        lista_puntos=[]
-        punto=-1
+        lista = sorted(lista, key=itemgetter(1))
+        __, max_y = max(lista, key=itemgetter(1))
+        __, min_y = min(lista, key=itemgetter(1))
+        lim = len(lista)
+        copia = img
+        lista_puntos = []
+        punto = -1
 
         ##primera linea
         for i in range(lim):
-            x,y=lista[i]
-            if(y==min_y):
-                copia[x,y]=0
-                lista_puntos.append((x,y))
+            x, y = lista[i]
+            if y == min_y:
+                copia[x, y] = 0
+                lista_puntos.append((x, y))
             else:
-                punto=i
+                punto = i
                 lista_puntos.pop()
                 break
 
-        #print(lista_puntos)
-
         ## ultima linea
         for i in range(lim-1,0,-1):
-            x,y=lista[i]
-            if(y==max_y):
-                copia[x,y]=0
-                lista_puntos.append((x,y))
+            x, y = lista[i]
+            if y == max_y:
+                copia[x, y] = 0
+                lista_puntos.append((x, y))
             else:
-                fin=i
-                lista_puntos.append((x,y))
-                copia[x,y]=0
+                fin = i
+                lista_puntos.append((x, y))
+                copia[x, y] = 0
                 break
         ant = min_y
-        #print(lista_puntos)
 
         ## el resto
-        for i in range(punto,fin-1,1):
-            x,y=lista[i]
-            if(y!=ant):
+        for i in range(punto, fin-1, 1):
+            x,y = lista[i]
+            if y != ant:
                 lista_puntos.append(lista[i-1])
                 lista_puntos.append(lista[i])
-                copia[x,y]=0
-                copia[lista[i-1]]=0
-                ant=y
+                copia[x, y] = 0
+                copia[lista[i-1]] = 0
+                ant = y
             else:
-                copia[x,y]=255
+                copia[x, y] = 255
 
         return lista_puntos, copia
 
     def excentricidad(self):
         centroide = self.find_centroid_cell()
         byw = cv2.imread('byw/imagen' + str(self.__name) + '.png')
-        bordes,bordes_img=self.obtener_bordes(byw,self.__col_lista)
-        lim=len(bordes)
-        dis_centro=[]
+        bordes, bordes_img = self.obtener_bordes(byw, self.__col_lista)
+        lim = len(bordes)
+        dis_centro = []
         for i in range(lim):
-            punto=bordes[i]
-            dis_centro.append(self.dist(centroide,punto))
+            punto = bordes[i]
+            dis_centro.append(self.dist(centroide, punto))
 
-        var=np.std(dis_centro)
-        maximo=max(dis_centro)
-        minimo=min(dis_centro)
-        exce=minimo/maximo
+        var = np.std(dis_centro)
+        maximo = max(dis_centro)
+        minimo = min(dis_centro)
+        exce = minimo/maximo
         #print('nombre=',self.__name)
         #print('la excentricidad es',exce)
         #print('la varianza es=',var)
-        bordes_img[centroide]=50
+        bordes_img[centroide] = 50
         #cv2.imshow('imagen',bordes_img)
         #cv2.waitKey(0)
-        return exce,var
-
+        return exce, var
 
     def principal(self):
-        sw=self.telofase()
-        if(sw==False):
-            exc,var=self.excentricidad()
-            if (exc>0.35 and exc<0.5):
-                print('la ',self.__name,'esta en anafase')
+        sw = self.telofase()
+        if sw is False:
+            exc, var = self.excentricidad()
+            if 0.35 < exc < 0.5:
+                #print('la ',self.__name,'esta en anafase')
                 self.guardar_imagen('anafase')
-
-            elif(exc>=0.5 and exc<0.65):
-                print('la ',self.__name,'esta en metafase')
+                return 3
+            elif 0.5 <= exc < 0.65:
+                #print('la ',self.__name,'esta en metafase')
                 self.guardar_imagen('metafase')
-
-            elif(exc>=0.65):
-                print('la ',self.__name,'esta en profase')
+                return 2
+            elif exc >= 0.65:
+                #print('la ',self.__name,'esta en profase')
                 self.guardar_imagen('profase')
-
+                return 1
             else:
-                print('la ',self.__name,'no pudo ser identificada')
+                #print('la ',self.__name,'no pudo ser identificada')
                 self.guardar_imagen('sin_clasificar')
+                return 0
+        else:
+            return 4
--- preparacion.py
+++ preparacion.py
@@ -1,135 +1,181 @@
 import numpy as np
 import cv2
+import errno
+import os
+import shutil
+import time
+
+
 from itertools import *
 from os import listdir
 from os.path import isfile, join
-import time
 from operator import itemgetter
 from clasificacion_fase import Clasificador
 
 
+def crear_directorios(name):
+    k="/"+name
+    if os.path.exists(k):
+        shutil.rmtree(os.getcwd() + k)
+    try:
+        os.makedirs(os.getcwd() + k)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
 def guardar_imagen(img, cont):
-    k='byw/imagen'+str(cont)+'.png'
+    k = 'byw/imagen'+str(cont)+'.png'
     cv2.imwrite(k, img)
 
+
 def cargar_imagenes():
-    mypath = 'test2'
+    mypath = 'test5'
     nombres = []
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     images = np.empty(len(onlyfiles), dtype=object)
     for n in range(0, len(onlyfiles)):
-        img=cv2.imread(join(mypath, onlyfiles[n]))
+        img = cv2.imread(join(mypath, onlyfiles[n]))
         images[n] = img
-        k=onlyfiles[n].split('.')
+        k = onlyfiles[n].split('.')
         nombres.append(k[0])
 
-    print('numero de imagenes=',len(images))
-    return images,nombres
+    print('numero de imagenes=', len(images))
+    return images, nombres
 
 
-def guardar_gris(img,cont,ruta):
+def guardar_gris(img, cont, ruta):
     k = ruta+'/imagen' + str(cont) + '.png'
     cv2.imwrite(k, img)
 
+
 def imagen_to_black_and_white(img,cont):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    guardar_gris(gray,cont,'gris')
-    sw=var_colores(gray,cont)
-    minimo,punto=min_colors(gray)
-    im_bw=change_color(gray,minimo)
-    return clear_edges(im_bw),punto,sw
+    guardar_gris(gray, cont, 'gris')
+    sw = var_colores(gray, cont)
+    minimo, punto = min_colors(gray)
+    im_bw = change_color(gray, minimo)
+    return clear_edges(im_bw), punto, sw
+
 
 def clear_edges(img):
-    return  cv2.medianBlur(img,5)
+    return cv2.medianBlur(img, 5)
 
-def change_color(img,minimo):
+
+def change_color(img, minimo):
     img[img < minimo + 30] = 0
     img[img > 0] = 255
     return img
+
 
 def dist(x, y):
     x1, y1 = x
     x2, y2 = y
     return np.sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2))
 
+
 def min_colors(img):
-    minimo=img[img>-1].min()
-    m,n=img.shape
-    indice=(0,0)
+    minimo = img[img>-1].min()
+    m, n = img.shape
+    indice = (0, 0)
     for i in range(m):
         for j in range(n):
-            if (img[i,j]==minimo):
-                indice=(i,j)
+            if img[i, j] == minimo:
+                indice = (i, j)
     #print(indice)
 
-    return minimo,indice
+    return minimo, indice
+
 
 def fill_holes(img):
     kernel = np.ones((2, 2), np.uint8)
     closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
     return closing
 
-def crear_Diccionario(image,num):
-    filas, colum, channels=image.shape
-    img,punto,sw=imagen_to_black_and_white(image,num)
+
+def crear_Diccionario(image, num):
+    filas, colum, channels = image.shape
+    img, punto, sw = imagen_to_black_and_white(image, num)
     lista = []
     puntos = []
-    if(sw==True):
-        img=fill_holes(img)
-        guardar_imagen(img,num)
-        elemi1=0
-        for i in product(range(0,filas),range(elemi1,colum)):
-            if(img[i]==0):
+    if sw is True:
+        img = fill_holes(img)
+        guardar_imagen(img, num)
+        elemi1 = 0
+        for i in product(range(0, filas), range(elemi1, colum)):
+            if img[i] == 0:
                 puntos.append(i)
-        limite=len(puntos)
-        for i in range(0,limite,1):
-            lista.append(dist(puntos[i],punto))
+        limite = len(puntos)
+        for i in range(0, limite, 1):
+            lista.append(dist(puntos[i], punto))
 
-    return lista,puntos,sw
+    return lista, puntos, sw
 
-def var_colores(img,name):
-    var=np.std(img)
+
+def var_colores(img, name):
+    var = np.std(img)
     #print('name=',name, 'var=',var)
-    if(var<40):
-        guardar_gris(img,name,'malas')
+    if var < 40:
+        guardar_gris(img, name, 'malas')
         return False
     else:
-        guardar_gris(img,name,'buenas')
+        guardar_gris(img, name, 'buenas')
         return True
 
 
-def principal(image,nombre):
-    dista,colores,sw=crear_Diccionario(image,nombre)
-    if(len(colores)>10 and sw==True):
-        classifier = Clasificador(image,nombre,dista,colores)
-        med=classifier.media()
-        var=classifier.varianza()
-        classifier.principal()
-        return (med,var)
+def principal(image, nombre):
+    dista, colores, sw = crear_Diccionario(image, nombre)
+    if len(colores) > 10 and sw is True:
+        classifier = Clasificador(image, nombre, dista, colores)
+        med = classifier.media()
+        var = classifier.varianza()
+        fase = classifier.principal()
+        return med, var, fase
     else:
         #print('name=',nombre,'princ no entro')
-        return (-1,-1)
+        return -1, -1, -1
 
 
-def main ():
+def show_phases(lista, malas):
+    print('resultados')
+    print('numero de celulas en profase:', lista[1])
+    print('numero de celulas en metafase:', lista[2])
+    print('numero de celulas en anafase:', lista[3])
+    print('numero de celulas en telofase:', lista[4])
+    print('numero de celulas sin clasificar', (lista[0]+malas))
+
+
+def main():
     initial = time.time()
-    imagenes,names=cargar_imagenes()
-    var= []
+    imagenes, names = cargar_imagenes()
+    crear_directorios('byw')
+    crear_directorios('gris')
+    crear_directorios('malas')
+    crear_directorios('buenas')
+    crear_directorios('profase')
+    crear_directorios('metafase')
+    crear_directorios('anafase')
+    crear_directorios('telofase')
+    crear_directorios('sin_clasificar')
+    var = []
     med = []
-    cont =0
-    malas=0
-    lenIma=len(imagenes)
+    fases_vec = [0, 0, 0, 0, 0]
+    cont = 0
+    malas = 0
+    lenIma = len(imagenes)
     for i in range(lenIma):
-        media,varianza=principal(imagenes[i],names[i])
-        if media==-1 and varianza==-1:
-            malas=malas+1
+        media, varianza, fase = principal(imagenes[i], names[i])
+        if media == -1 and varianza == -1:
+            malas = malas+1
         else:
-            cont=cont+1
-            var.append((varianza,names[i]))
-            med.append((media,names[i]))
-    print('malas=',malas)
-    var=sorted(var, key=itemgetter(0))
-    med= sorted(med, key=itemgetter(0))
+            fases_vec[fase] = fases_vec[fase]+1
+            cont = cont+1
+            var.append((varianza, names[i]))
+            med.append((media, names[i]))
+    print('malas=', malas)
+    show_phases(fases_vec, malas)
+    var = sorted(var, key=itemgetter(0))
+    med = sorted(med, key=itemgetter(0))
     k = len(var)
     """""
     print('ordenadas por varianza')
@@ -141,10 +187,9 @@ def main ():
         print(med[i])
     
     """""
-
-    End = time.time()
-    print('tiempo=',End-initial)
+    end = time.time()
+    print('tiempo=', end-initial)
     print('fin')
 
-main()
 
+main()
